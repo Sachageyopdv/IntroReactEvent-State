83ef7d8072156c663d38fca4f2f04a056403ebb8
Sachageyo <s.nguyenvanbau@gmail.com>
2024-01-10 15:05:14 +0100

    Moved the button in the counter component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import Counter from "./conponents/counter";
+import './style/style.css'
 
 const App = () => {
   const [theme, setTheme] = useState("dark");
-  const [val, setVal] = useState(0);
 
   const handleCLick = () => {
     alert("Tas clické");
@@ -21,14 +21,6 @@ const App = () => {
     });
   };
 
-  const addCounter = () => {
-    setVal(val + 1);
-  };
-
-  const subCounter = () => {
-    if (val > 0) setVal(val - 1);
-  };
-
   return (
     <>
       <h1
@@ -41,9 +33,7 @@ const App = () => {
       <button onClick={handleCLick}>CLICK</button>
       <button onMouseOver={() => console.log("HELLO")}>CLICK2</button>
       <button onClick={handleTheme}>Change BG COLOR</button>
-      <Counter val={val} />
-      <button onClick={subCounter}>-1</button>
-      <button onClick={addCounter}>+1</button>
+      <Counter />
     </>
   );
 };
--- src/conponents/counter.js
+++ src/conponents/counter.js
@@ -1,6 +1,23 @@
+import { useState } from "react";
 
-const Counter = ({ val }) => {
-  return <div className="counter">{val}</div>;
+const Counter = () => {
+  const [val, setVal] = useState(0);
+
+  const addCounter = () => {
+    setVal(val + 1);
+  };
+
+  const subCounter = () => {
+    if (val > 0) setVal(val - 1);
+  };
+
+  return (
+    <>
+      <div className="counter">{val}</div>
+      <button onClick={subCounter}>-1</button>
+      <button onClick={addCounter}>+1</button>
+    </>
+  );
 };
 
 export default Counter;
